9cde4ac12f5e77965a76e5ffe2b35379d1170b78
ShaeS <shaescotten@gmail.com>
2016-02-16 12:13:52 -0800

    added more CRUD

--- controller/games.php
+++ controller/games.php
@@ -1,12 +1,16 @@
 <?php
 include("../model/gamesdb.php");
 
-if($_POST['method'] == "insert"){
+if($_POST['method'] == "add"){
     insert_game();   
 }
 
 if($_POST['method'] == "getall"){
     get_games();   
+}
+
+if($_POST['method'] == "update"){
+    update_game();   
 }
 
 
